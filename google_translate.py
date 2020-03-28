c64344b56807d1abafea633e33a7020147a94b07
Rahul Noronha <rahulnor@gmail.com>
2020-03-28 19:32:27 +0530

    Update google_translate.py

--- google_translate.py
+++ google_translate.py
@@ -1,4 +1,7 @@
 import googletrans
 from googletrans import Translator
-translated = Translator().translate('hindi.', src='en', dest='hin')
-print(translated.text)
+languages=[["English","en"],["Kannada","kn"],["Hindi","hi"],["Tamil","ta"],["Telugu","te"]]
+
+for language in languages:
+    translated = Translator().translate(language[0]+" is chosen\n", src='en', dest=language[1])
+    print(f" {translated.text} ")
