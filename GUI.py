10ae6f7c3cea4a1c7f194a62a3e40f1aaa941c92
riyasavant <46701530+riyasavant@users.noreply.github.com>
2020-05-12 02:46:05 +0530

    Update GUI.py
    Changed main GUI code along with translation

--- GUI.py
+++ GUI.py
@@ -1,109 +1,99 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Apr  2 11:29:49 2020
-
-@author: sandesh
-"""
-
-import tkinter as tk
 from tkinter import *
-#import googletranslate as gtrans
 from PIL import ImageTk, Image
-import os
-#import sample2 as img
+import database
+import webbrowser
+import translate
+window = Tk()
+window.geometry("1500x800")
+window.state('zoomed')
+trans_array = ["KARNATAKA FARMER PORTAL", "How long have you been searching for the right", "place to buy and sell at the same time", "Farmers", "Uplift your profit", "Buyers", "Find all you need in one place"]
+window.resizable(0, 0)
+window_canvas=Canvas(window,height=800,width=1600)
 
-window = tk.Tk()
-window.geometry("1500x800")  # geometry of the window
-window.title(" FARMERS PORTAL ")  # title of the window
-title_canvas=tk.Canvas(window,bg="#2C3E50",width=1500,height=180)
-title = tk.Label(title_canvas, text=" WELCOME TO FARMER'S PORTAL ", font=('Calibri', 35), fg="#F4F6F7",
-                 bg="#2C3E50",anchor="center",height=3)
-title.grid(padx=170,pady=2) 
-title_canvas.create_text(1338, 65, text="Choose your preferred language",font=('Calibri', 15),fill="#ffffff")
 
-variable = tk.StringVar(window)
-OptionList = ["English", "ಕನ್ನಡ", "हिंदी", "தமிழ்", "తెలుగు"]  # dropdown
+main_canvas=Canvas(window_canvas,height=700,width=1600,background="bisque")
+blur_image = ImageTk.PhotoImage(file=r"C:\Users\Riya Savant\PycharmProjects\ADA\image\blur.png")
+main_canvas.create_image(700,300, image=blur_image)
+icon_image = PhotoImage(file=r"C:\Users\Riya Savant\PycharmProjects\ADA\image\ecology.png")
+icon_image = icon_image.subsample(10, 10)
+main_canvas.create_image(150, 70, image=icon_image)
+
+rectangle = main_canvas.create_rectangle(1200, 200, 1450, 600,outline="#C0392B",width=3)#
+title=main_canvas.create_text(580, 75, text=trans_array[0], font=('monospace', 37),fill="#F7DC6F")
+
+signin_button=Button(main_canvas,text="Sign in",font=('monospace',15,'underline'), command=database.signin)
+signin_button.configure(fg="#D4AC0D",bg="#000000",bd=2,highlightbackground="#F7DC6F",relief=SOLID, activebackground='#000000',activeforeground='#000000')
+signin_button=main_canvas.create_window(1385, 75, window=signin_button ,width=100)
+
+def Web():
+    webbrowser.open('https://farmers-portal.github.io/farmsite/')
+
+help_button=Button(window_canvas,text="Help",font=('monospace',15,'underline'), command=Web)
+help_button.configure(fg="#D4AC0D",bg="#000000",bd=2,highlightbackground="#f7dc6f",relief=SOLID, activebackground='#000000',activeforeground='#000000')
+help_button=window_canvas.create_window(1280, 75, window=help_button ,width=100)
+
+delivery_button=Button(window_canvas,text="Delivery",font=('monospace',15,'underline'), command=database.delivery)
+delivery_button.configure(fg="#D4AC0D",bg="#000000",bd=2,highlightbackground="#f7dc6f",relief=SOLID, activebackground='#000000',activeforeground='#000000')
+delivery_button=window_canvas.create_window(1180, 75, window=delivery_button ,width=100)
+
+head_top = main_canvas.create_text(570, 230, text=trans_array[1], font=('Britannic Bold', 36),fill="#ffffff")
+head_bottom = main_canvas.create_text(570, 280, text=trans_array[2]+'?', font=('Britannic Bold', 36),fill="#ffffff")
+sub_head_one = main_canvas.create_text(550, 530, text=trans_array[3]+' - '+trans_array[4]+'.', font=('monospace', 20),fill="#ffffff")
+sub_head_two = main_canvas.create_text(550, 580, text=trans_array[5]+' - '+trans_array[6]+'.', font=('monospace', 20),fill="#ffffff")
+main_canvas.create_text(1330, 330, text="Add the about\n info here",font=('monospace', 24),fill="#ffffff")
+regfar_icon = PhotoImage(file=r"C:\Users\Riya Savant\PycharmProjects\ADA\image\agriculture.png")
+regfar_icon = regfar_icon.subsample(4,4)
+regfar_button=Button(main_canvas,text="     Farmer",image=regfar_icon,font=('monospace',20),anchor=W,compound='left', command=database.farmers)
+regfar_button.configure(fg="#000000",bg="#ffffff", activebackground='#ffffff',activeforeground='#000000')
+regfar_button=main_canvas.create_window(650, 395, window=regfar_button ,width=200,height=50)
+
+regbuy_icon = PhotoImage(file=r"C:\Users\Riya Savant\PycharmProjects\ADA\image\shoppingcart.png")
+regbuy_icon = regbuy_icon.subsample(4,4)
+regbuy_button=Button(main_canvas,text="     Buyer",image=regbuy_icon,font=('monospace',20),anchor=W,compound='left', command=database.users)
+regbuy_button.configure(fg="#000000",bg="#ffffff", activebackground='#ffffff',activeforeground='#000000')
+regbuy_button=main_canvas.create_window(430, 395, window=regbuy_button ,width=200,height=50)
+variable = StringVar(window)
+OptionList = ["English", "ಕನ್ನಡ", "हिंदी"]  # dropdown
 variable.set(OptionList[0])
-dropdown = tk.OptionMenu(title_canvas, variable, *OptionList)#,command=gtrans.translate(OptionList[1]))
-dropdown.config(width=17, font=('Calibri', 15), fg="black", bg="#F4F6F7",padx=15,pady=5)
-dropdown=title_canvas.create_window(1345, 105, window=dropdown)
-#gtrans.translate(OptionList[i])
-title_canvas.pack(fill="both",expand=1)  
-                       
-                         
-main_canvas=tk.Canvas(window,bg="#5D6D7E",width=1500,height=642)
+dropdown = OptionMenu(window_canvas, variable, *OptionList)#,command=gtrans.translate(OptionList[1]))
+dropdown.config(width=12, font=('monospace', 15,'bold'), fg="black", bg="#CACFD2",padx=15,pady=5)
+dropdown=window_canvas.create_window(1345, 740, window=dropdown)
+window_canvas.pack(fill="both",expand=True)
+main_canvas.pack( anchor=N,expand=True)
+def fun():
+    if(variable.get()=="हिंदी"):
+        new_array = translate.trans(trans_array, 'en', 'hi')
+        main_canvas.itemconfig(title, text=new_array[0].text)
+        main_canvas.itemconfig(head_top, text=new_array[1].text)
+        main_canvas.itemconfig(head_bottom, text=new_array[2].text+'?')
+        main_canvas.itemconfig(sub_head_one, text=new_array[3].text+'  -  '+new_array[4].text+'.')
+        main_canvas.itemconfig(sub_head_two, text=new_array[5].text+'  -  '+new_array[6].text+'.')
+    elif(variable.get()=="ಕನ್ನಡ"):
+        new_array = translate.trans(trans_array, 'en', 'kn')
+        main_canvas.itemconfig(title, text=new_array[0].text)
+        main_canvas.itemconfig(head_top, text=new_array[1].text)
+        main_canvas.itemconfig(head_bottom, text=new_array[2].text + '?')
+        main_canvas.itemconfig(sub_head_one, text=new_array[3].text + '  -  ' + new_array[4].text + '.')
+        main_canvas.itemconfig(sub_head_two, text=new_array[5].text + '  -  ' + new_array[6].text + '.')
+    else:
+        main_canvas.itemconfig(title, text=trans_array[0])
+        main_canvas.itemconfig(head_top, text=trans_array[1])
+        main_canvas.itemconfig(head_bottom, text=trans_array[2] + '?')
+        main_canvas.itemconfig(sub_head_one, text=trans_array[3] + '  -  ' + trans_array[4] + '.')
+        main_canvas.itemconfig(sub_head_two, text=trans_array[5] + '  -  ' + trans_array[6] + '.')
 
-#img.imag(main_canvas)
-                      
-
-button_canvas=Canvas(main_canvas,bg="#B3B6B7",height=965,width=300)
-                     
-def register():
-#    register_frame=tk.Frame(main_canvas,bg="#D7DBDD",width=700,height=400).grid(padx=400,pady=123)
-    reg_window=tk.Tk()
-    reg_window.geometry("900x500+250+200")
-    reg_window.title("Registration")
-    reg_window.resizable(0,0)
-    reg_window.configure(bg="#D7DBDD")
-                         
-    register_label=tk.Label(reg_window,text="Enter your details below",fg="#000000",bg="#D7DBDD",font=('Calibri',15),anchor=N).grid(padx=330,pady=10)
-
-    reg_canvas=tk.Canvas(reg_window,width=900,height=450,bg="#D7DBDD",highlightthickness=0)
-
-    fullname_label=tk.Label(reg_window,text="Full Name",fg="#000000",bg="#D7DBDD",font=('Calibri',13))
-    fullname_label=reg_canvas.create_window(300,50,window=fullname_label)
-    name_entry=tk.Entry(reg_canvas,font=15)
-    name_entry=reg_canvas.create_window(470,50,window=name_entry)
-
-    crop_label=tk.Label(reg_window,text="Crop",fg="#000000",bg="#D7DBDD",font=('Calibri',13))
-    crop_label=reg_canvas.create_window(300,90,window=crop_label)
-    crop_entry=tk.Entry(reg_canvas,font=15)
-    crop_entry=reg_canvas.create_window(470,90,window=crop_entry)
-
-    email_label=tk.Label(reg_window,text="Email ID",fg="#000000",bg="#D7DBDD",font=('Calibri',13))
-    email_label=reg_canvas.create_window(300,130,window=email_label)
-    email_entry=tk.Entry(reg_canvas,font=15)
-    email_entry=reg_canvas.create_window(470,130,window=email_entry)
-
-    phone_label=tk.Label(reg_window,text="Mobile number",fg="#000000",bg="#D7DBDD",font=('Calibri',13))
-    phone_label=reg_canvas.create_window(300,170,window=phone_label)
-    phone_entry=tk.Entry(reg_canvas,font=15)
-    phone_entry=reg_canvas.create_window(470,170,window=phone_entry)
-
-    submit_button=tk.Button(reg_window,text="Submit",highlightcolor="#000000",highlightthickness=2,width=10,fg="#000000",bg="#ffffff")
-    submit_button=reg_canvas.create_window(500,400,window=submit_button)    
-
-    return_button=tk.Button(reg_window,text="Return",highlightcolor="#000000",highlightthickness=2,width=10,fg="#000000",bg="#ffffff",command=reg_window.destroy)
-    return_button=reg_canvas.create_window(400,400,window=return_button)
-    
-    reg_canvas.grid()
-
-    reg_window.mainloop()
-
-button_canvas=main_canvas.create_window(1383,178,window=button_canvas)
-
-register_button=Button(main_canvas,text="Register",font='Calibri',command=register)
-register_button=main_canvas.create_window(1385, 50, window=register_button, anchor=CENTER,width=100)
-info_button=Button(main_canvas,text="Info",font='Calibri',padx=40,pady=0)
-info_button=main_canvas.create_window(1385, 125, window=info_button , anchor=CENTER,width=100)
-help_button=Button(main_canvas,text="Help",font='Calibri',padx=40,pady=0)
-help_button=main_canvas.create_window(1385, 200, window=help_button, anchor=CENTER,width=100)
- 
-main_canvas.create_text(130, 50, text="Select your location :",font=('Calibri', 15),fill="#ffffff")
-v=tk.IntVar()
-v.set(2)
-radiobutton=tk.Radiobutton(main_canvas,text="Bangalore rural",bg="#5D6D7E",fg="#000000",font=('Calibri', 13),variable=v,value=1)
-main_canvas.create_window(150, 90, window=radiobutton) 
-radiobutton=tk.Radiobutton(main_canvas,text="Udupi",bg="#5D6D7E",fg="#000000",font=('Calibri', 13),variable=v,value=2)
-main_canvas.create_window(117, 125, window=radiobutton) 
-radiobutton=tk.Radiobutton(main_canvas,text="Bijapur",bg="#5D6D7E",fg="#000000",font=('Calibri', 13),variable=v,value=3)
-main_canvas.create_window(119, 160, window=radiobutton) 
-
-#farm_image = ImageTk.PhotoImage(file="farm.gif")
-#main_canvas.create_image(200, 200, image=farm_image, anchor=NW)
-
-
-main_canvas.pack(fill="both",expand=1)
-         
-
+trans_butt = Button(window, text='Translate',width='15', command=fun)
+trans_butt.place(x=1290, y=780)
+icon1 = PhotoImage(file=r"C:\Users\Riya Savant\PycharmProjects\ADA\image\earth.png")
+icon1 = icon1.subsample(5,5)
+img = Label(window, image=icon1)
+img.place(x=700,y=725)
+icon2 = PhotoImage(file=r"C:\Users\Riya Savant\PycharmProjects\ADA\image\farmer.png")
+icon2 = icon2.subsample(5,5)
+img = Label(window, image=icon2)
+img.place(x=400,y=725)
+icon3 = PhotoImage(file=r"C:\Users\Riya Savant\PycharmProjects\ADA\image\growth.png")
+icon3 = icon3.subsample(5,5)
+img = Label(window, image=icon3)
+img.place(x=1000,y=725)
 window.mainloop()
