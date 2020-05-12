9a55be4b49916d558f2f22a8de93b7699517d54b
Sandesh <42542821+San411@users.noreply.github.com>
2020-05-13 00:40:38 +0530

    Update email module.py

--- email module.py
+++ email module.py
@@ -1,17 +1,33 @@
-import os
 import smtplib
+import string
 
-# email_password=os.environ.get('')
 
-with smtplib.SMTP('smtp.gmail.com',587) as smtp:
-    smtp.ehlo()
-    smtp.starttls()
-    smtp.ehlo()
 
-    smtp.login('karfarmersportal@gmail.com','GENERATED_PASSWORD')
+def email(email_id,verification_code):
+    with smtplib.SMTP('smtp.gmail.com',587) as smtp:
+        smtp.ehlo()
+        smtp.starttls()
+        smtp.ehlo()
 
-    subject='TEXT'
-    body='TEXT'
-    message=f'Subject: {subject}\n\n{body}'
+        smtp.login('karfarmersportal@gmail.com','zfaudsxcqrpkqzoz')#'iecsydcwgyifaafw')
 
-    smtp.sendmail('karfarmersportal@gmail.com','karfarmersportal@gmail.com',message)
+        subject='Verification code'
+        body=verification_code
+        message=f'Subject: {subject}\n\n{body}'
+
+        smtp.sendmail('karfarmersportal@gmail.com',email_id,message)
+#
+def actualemail(email_id,final_price,name):
+    with smtplib.SMTP('smtp.gmail.com',587) as smtp:
+        smtp.ehlo()
+        smtp.starttls()
+        smtp.ehlo()
+
+        smtp.login('karfarmersportal@gmail.com','zfaudsxcqrpkqzoz')#'iecsydcwgyifaafw')
+
+        subject='Purchase Details'
+        body = 'Thank you for shopping with us ' + name + '! Your final amount is Rs. ' + str(final_price)
+        message=f'Subject: {subject}\n\n{body}'
+
+        smtp.sendmail('karfarmersportal@gmail.com',email_id,message)
+
